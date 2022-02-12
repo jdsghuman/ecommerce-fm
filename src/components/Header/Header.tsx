8bf8a12de69d45e0c2e737f08da582572e9a477d
JD Ghuman <jdshguman@gmail.com>
2022-02-11 22:48:54 -0600

    Add SASS and styles files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { FaShoppingCart } from "react-icons/fa";
+import Container from "src/components/Container";
+import styles from "./Header.module.scss";
+
+const Header = ({}) => {
+  return (
+    <header className={styles.header}>
+      <Container className={styles.headerContainer}>
+        <p className={styles.headerTitle}>HTX Farmer&apos;s Market</p>
+        <p className={styles.headerCart}>
+          <button className="snipcart-checkout">
+            <FaShoppingCart />
+            <span className="snipcart-total-price">$0.00</span>
+          </button>
+        </p>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
